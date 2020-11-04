b20cfccd0b603c4d120bfa04477802546d48e1b6
DenisPlaksia <denisplaksia@gmail.com>
2020-11-04 13:43:33 +0300

    Add-player-controller

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float walkingSpeed = 7.5f;
+    public float runningSpeed = 11.5f;
+    public float jumpSpeed = 8.0f;
+}
