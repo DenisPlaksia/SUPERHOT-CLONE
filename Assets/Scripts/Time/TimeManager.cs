b0375e699bd3ce61ed980d486453fa6a0ad74678
DenisPlaksia <denisplaksia@gmail.com>
2020-11-07 10:47:44 +0300

    #1 -Add-player-controller and time manager

---
+++ Assets/Scripts/Time/TimeManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+
+/*Denis Plaksia*/
+public class TimeManager : MonoBehaviour
+{
+    private float slowdownFactor = 0.005f;
+    public void StopTime() => Time.timeScale = slowdownFactor;
+
+    public void RunTime() => Time.timeScale = 1f;
+}
